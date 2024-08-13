4d8dc1b5223f39ad79e540747d021b2be31b105a
Marcos Santos <marcosdiego.ns01@gmail.com>
2024-08-13 18:30:10 -0500

    feat: integrate Prisma for database management
    Added Prisma schema, updated dependencies, and initialized Prisma Client. This enables interactions with a PostgresSQL database, enhancing our application's database capabilities.

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
-import fastify from "fastify"
+import fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
 
-export const app = fastify()
+export const app = fastify();
+
+const prisma = new PrismaClient();
+
+prisma.user.create({
+  data: {
+    name: "Marcos Diego",
+    email: "marcosdiego.ns01@gmail.com",
+  },
+});
