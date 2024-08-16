72b490bb1d1f9608c86e6bcf0f387c6d59256cd4
Marcos Santos <marcosdiego.ns01@gmail.com>
2024-08-16 12:15:26 -0500

    refactor: introduce factory methods for UseCases
    Extract the creation logic of AuthenticateUseCase and RegisterUseCase into separate factory functions. This change improves the separation of concerns and enhances code maintainability by centralizing the instantiation logic.

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,8 +1,7 @@
 import { z } from "zod";
 import { FastifyReply, FastifyRequest } from "fastify";
-import { RegisterUseCase } from "@/use-cases/register";
-import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
 import { UserAlreadyExistsError } from "@/use-cases/errors/user-already-exists-error";
+import { makeRegisterUseCase } from "@/use-cases/factory/make-register-use-case";
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -14,9 +13,7 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
   const { name, email, password } = registerBodySchema.parse(request.body);
 
   try {
-    const prismaUsersRepository = new PrismaUsersRepository();
-    const registerUserCase = new RegisterUseCase(prismaUsersRepository);
-
+    const registerUserCase = makeRegisterUseCase();
     await registerUserCase.execute({ name, email, password });
   } catch (err) {
     if (err instanceof UserAlreadyExistsError) {
