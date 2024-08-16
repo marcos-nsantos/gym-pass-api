c8f01a57765ccc979f981f7daa518a46966e00ee
Marcos Santos <marcosdiego.ns01@gmail.com>
2024-08-16 10:45:02 -0500

    refactor: extract setup logic to beforeEach in tests
    Refactored test setup by moving repeated initializations to beforeEach in both register and authenticate use cases. This reduces redundancy and improves code maintainability.

--- src/use-cases/authenticate.spec.ts
+++ src/use-cases/authenticate.spec.ts
@@ -2,9 +2,17 @@ import { InMemoryUsersRepository } from "@/repositories/in-memory/in-memory-user
 import { AuthenticateUseCase } from "@/use-cases/authenticate";
 import { InvalidCredentialsError } from "@/use-cases/errors/invalid-credentials-error";
 import { hash } from "bcryptjs";
-import { describe, expect, it } from "vitest";
+import { beforeEach, describe, expect, it } from "vitest";
+
+let usersRepository: InMemoryUsersRepository;
+let sut: AuthenticateUseCase;
 
 describe("Authenticate Use Case", () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository();
+    sut = new AuthenticateUseCase(usersRepository);
+  });
+
   it("should be able to authenticate", async () => {
     const usersRepository = new InMemoryUsersRepository();
     const sut = new AuthenticateUseCase(usersRepository);
