a03d450dbfa898ce8ae5cfa23a770e978967be2a
Marcos Santos <marcosdiego.ns01@gmail.com>
2024-08-16 12:33:12 -0500

    feat: add a get user profile use case
    Implement GetUserProfileUseCase to retrieve user profiles by ID. Includes error handling for non-existent users. Added corresponding unit tests and repository method implementations.

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -3,6 +3,10 @@ import { Prisma, User } from "@prisma/client";
 import { UsersRepository } from "@/repositories/users-repository";
 
 export class PrismaUsersRepository implements UsersRepository {
+  findById(id: string): Promise<User | null> {
+    throw new Error("Method not implemented.");
+  }
+
   async create(data: Prisma.UserCreateInput): Promise<User> {
     const user = await prisma.user.create({ data });
     return user;
