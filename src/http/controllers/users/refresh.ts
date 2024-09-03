a094336c350c6303e53885ce05c136d4d9013fc0
Marcos Santos <marcos.diego.ns@outlook.com>
2024-09-03 16:12:20 -0500

    feat: implement user roles with authorization
    Add support for user roles (ADMIN, MEMBER) and enforce role-based access control. Introduce `verifyUserRole` middleware to restrict actions based on role and update routes to use the middleware. Include roles in JWT tokens for consistent authorization checks.

--- src/http/controllers/users/refresh.ts
+++ src/http/controllers/users/refresh.ts
@@ -3,17 +3,17 @@ import { FastifyReply, FastifyRequest } from "fastify";
 export async function refresh(request: FastifyRequest, reply: FastifyReply) {
   await request.jwtVerify({ onlyCookie: true });
 
+  const { role } = request.user;
+
   const token = await reply.jwtSign(
-    {},
+    { role },
     {
-      sign: {
-        sub: request.user.sub,
-      },
+      sign: { sub: request.user.sub },
     },
   );
 
   const refreshToken = await reply.jwtSign(
-    {},
+    { role },
     {
       sign: {
         sub: request.user.sub,
