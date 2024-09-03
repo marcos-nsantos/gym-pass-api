55735220f80b7c39d939b14f447a523b8d958e22
Marcos Santos <marcos.diego.ns@outlook.com>
2024-09-03 14:55:29 -0500

    feat(auth): add token refresh endpoint and tests
    Introduce an endpoint for refreshing JWT tokens along with corresponding integration tests. This ensures users can securely refresh their tokens without re-authenticating.

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,10 +3,13 @@ import { register } from "@/http/controllers/users/register";
 import { authenticate } from "@/http/controllers/users/authenticate";
 import { profile } from "@/http/controllers/users/profile";
 import { verifyJwt } from "@/http/middlewares/verify-jwt";
+import { refresh } from "@/http/controllers/users/refresh";
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post("/users", register);
   app.post("/sessions", authenticate);
+
+  app.patch("/token/refresh", refresh);
 
   // Authenticated
   app.get("/me", { onRequest: [verifyJwt] }, profile);
